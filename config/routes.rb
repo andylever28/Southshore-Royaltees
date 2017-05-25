b7487e5f562a056e124c532782f3911a5bc6ea98
Andrew Lever <andylever28@gmail.com>
2017-05-24 22:36:18 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'main/home'
+	root 'main#home'
+  get 'main/about'
+
+  get 'main/portfolio'
+
+  get 'main/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
